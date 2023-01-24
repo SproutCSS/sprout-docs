e292430f9567933aafd2626ccf7aada1a01a059c
dejmedus <email96123@gmail.com>
2023-01-24 01:16:32 -0700

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import clsx from "clsx";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import styles from "./index.module.css";
+
+export default function Home(): JSX.Element {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <Layout title={`${siteConfig.title}`} description="Notebook">
+      <HomepageHeader />
+      <main></main>
+    </Layout>
+  );
+}
+
+function HomepageHeader() {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
+      <div className="container">
+        <h1 className="hero__title">{siteConfig.title}</h1>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons}></div>
+      </div>
+    </header>
+  );
+}
