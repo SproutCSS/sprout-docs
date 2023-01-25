f869025e423ccb63a9472fc0e25020313167a443
dejmedus <email96123@gmail.com>
2023-01-25 00:05:34 -0700

    add heya page

---
+++ src/pages/heya.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import styles from "./index.module.css";
+
+export default function Heya(): JSX.Element {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <Layout
+      title={`${siteConfig.title}`}
+      description="A Tiny CSS Component Library"
+    >
+      <main>
+        <div className={styles.heroBanner}>
+          <h1 className={styles.heya}>❤️🌱</h1>
+          <h2>You're The Best</h2>
+        </div>
+      </main>
+    </Layout>
+  );
+}
