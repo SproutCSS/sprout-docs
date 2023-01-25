77c35d91ddf03e09f4ebbc761f8e79e1df2d53ea
dejmedus <email96123@gmail.com>
2023-01-25 00:08:46 -0700

    change layouts filetype to tsx

---
+++ src/components/layout.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import Tabs from "@theme/Tabs";
+import TabItem from "@theme/TabItem";
+import CodeBlock from "@theme/CodeBlock";
+import "../css/layout.css";
+import "../css/sprout.css";
+
+export default function Layout({ title, options, preview, html, jsx }) {
+  return (
+    <>
+      <h3>{title}</h3>
+      <Options options={options} />
+      <Code preview={preview} html={html} jsx={jsx} />
+    </>
+  );
+}
+
+function Code({ preview, html, jsx }) {
+  return (
+    <Tabs>
+      <TabItem value="preview" label="Preview" default>
+        {preview.map((row) => {
+          return <div className="flex">{row}</div>;
+        })}
+      </TabItem>
+      <TabItem value="html" label="HTML">
+        <CodeBlock language="html">{html}</CodeBlock>
+      </TabItem>
+      <TabItem value="jsx" label="JSX">
+        <CodeBlock language="jsx">{jsx}</CodeBlock>
+      </TabItem>
+    </Tabs>
+  );
+}
+
+function Options({ options }) {
+  return (
+    <div className="options">
+      <div className="class">{options.desc}</div>
+      {options.options.map((option) => {
+        return (
+          <div className="row">
+            <div className="name">{option.name}</div>
+            <div className="choices">
+              {option.choices.split(" ").map((choice) => {
+                return <Class class={choice} />;
+              })}
+            </div>
+          </div>
+        );
+      })}
+      <br />
+    </div>
+  );
+}
+
+function Class(props) {
+  return (
+    <button
+      className="btn"
+      onClick={() => navigator.clipboard.writeText(props.class)}
+    >
+      {props.class}
+    </button>
+  );
+}
