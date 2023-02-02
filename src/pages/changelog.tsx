dd9390eb2410c7af7fec782982ac69ae464de42c
dejmedus <email96123@gmail.com>
2023-02-01 23:08:24 -0700

    update changelog for 1.3.0

--- src/pages/changelog.tsx
+++ src/pages/changelog.tsx
@@ -1,15 +1,51 @@
 import React from "react";
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 import styles from "./index.module.css";
 
 export default function Changelog(): JSX.Element {
-  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout title="Change Log" description="A Tiny CSS Component Library">
       <main className={styles.changelogContainer}>
         <h1>Change Log</h1>
         <div className={styles.changelog}>
+          <h2>1.3.0</h2>
+          <h3>Whats Changed</h3>
+          <ul>
+            <li>
+              The SproutCSS module is no longer needed in production. It can now
+              be
+              <a href="/docs/intro"> installed as a dev dependency.</a>
+            </li>
+            <li>
+              More customization options.
+              <a href="/docs/Components/Buttons">
+                {" "}
+                Colors, hover styles, and borders.
+              </a>
+            </li>
+            <li>
+              If a component color is not specified, Sprout will now default to
+              the primary color
+            </li>
+          </ul>
+          <h3>Whats New</h3>
+          <ul>
+            <li>
+              <em style={{ backgroundColor: "rgb(255, 251, 0, 0.2)" }}>
+                No more unused styling 🥳
+              </em>{" "}
+              Sprout now
+              <a href="/docs/custom-stylesheet">
+                {" "}
+                generates a custom css file.
+              </a>
+            </li>
+            <li>
+              New color options. Choose from primary, black, grey, red, orange,
+              yellow, green, blue, purple, and pink.
+            </li>
+          </ul>
+          <br />
           <h2>1.2.0</h2>
           <h3>Whats Changed</h3>
           <ul>
@@ -47,7 +83,7 @@ export default function Changelog(): JSX.Element {
           <h3>Whats New</h3>
           <ul>
             <li>
-              A <a href="/docs/Components/Buttons">button</a> 🎉
+              A <a href="/docs/Components/Buttons">button</a> ✨
             </li>
           </ul>
         </div>
